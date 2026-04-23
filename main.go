832842a26567c85735f901fead49d4a2cdd6dc06
nova34tkj4 <novahariyakusuma@gmail.com>
2026-04-23 20:31:44 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "selamat malam semuanya")
+    fmt.Fprintf(w, "selamat malam semuanya & sehat selalu")
 }
 
 func main() {
